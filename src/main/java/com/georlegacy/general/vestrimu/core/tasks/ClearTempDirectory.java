47123f37ac2ca9debb7cb71725288567cf48ba6a
James <615283@users.noreply.github.com>
2018-08-03 18:04:23 +0100

    temp

--- src/main/java/com/georlegacy/general/vestrimu/core/tasks/ClearTempDirectory.java
+++ src/main/java/com/georlegacy/general/vestrimu/core/tasks/ClearTempDirectory.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Objects;
 
 public class ClearTempDirectory implements Runnable {
 
@@ -16,21 +15,41 @@ public class ClearTempDirectory implements Runnable {
     public void run() {
         long currentMillis = System.currentTimeMillis();
         Vestrimu.getLogger().info("Clearing disused files from temp directory");
-        for (File tmpFile : Objects.requireNonNull(tmpDir.listFiles(file -> !file.isDirectory()))) {
-            Vestrimu.getLogger().debug("found file");
-            try {
-                BasicFileAttributes attrs = Files.readAttributes(tmpFile.toPath(), BasicFileAttributes.class);
-                long creationMillis = attrs.creationTime().toMillis();
-                long accessMillis = attrs.lastAccessTime().toMillis();
-
-                if ((creationMillis < (currentMillis - (2 * 1000 * 60 * 60))) && (accessMillis < (currentMillis - (2 * 1000 * 60 * 60))))
-                    if (tmpFile.delete())
-                        Vestrimu.getLogger().info("Successfully deleted temp file " + tmpFile.getName());
-                    else
-                        Vestrimu.getLogger().info("Failed to delete temp file " + tmpFile.getName());
-            } catch (IOException ex) {
-                ex.printStackTrace();
+        for (File tmpFile : tmpDir.listFiles()) {
+            if (tmpFile.isDirectory()) {
+                proccessDirectory(tmpFile);
+            } else {
+                proccessFile(tmpFile);
             }
+        }
+    }
+
+    private void proccessDirectory(File directory) {
+        for (File tmpFile : directory.listFiles()) {
+            if (tmpFile.isDirectory()) {
+                proccessDirectory(tmpFile);
+            } else {
+                proccessFile(tmpFile);
+            }
+        }
+    }
+
+    private void proccessFile(File file) {
+        try {
+            long currentMillis = System.currentTimeMillis();
+
+            BasicFileAttributes attrs = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+            long creationMillis = attrs.creationTime().toMillis();
+            long accessMillis = attrs.lastAccessTime().toMillis();
+
+            if ((creationMillis < (currentMillis - (2 * 1000 * 60 * 60))) && (accessMillis < (currentMillis - (2 * 1000 * 60 * 60))))
+                if (file.delete())
+                    Vestrimu.getLogger().info("Successfully deleted temp file " + file.getName());
+                else
+                    Vestrimu.getLogger().info("Failed to delete temp file " + file.getName());
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 
